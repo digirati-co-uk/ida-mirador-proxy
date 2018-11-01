c6f961748fbcacee51c2a8e37ba960cf23e01762
Matt McGrattan <matt.mcgrattan@digirati.com>
2018-11-01 14:12:32 +0000

    Logging. Removed unused shellscript.

--- mirador.py
+++ mirador.py
@@ -5,6 +5,8 @@ from flask import request, send_from_directory, url_for
 from flask_caching import Cache
 import mirador_settings
 from flask_cors import CORS
+import sys
+import logging
 
 
 def cache_key():
@@ -13,7 +15,7 @@ def cache_key():
 
 app = flask.Flask(__name__, static_folder="build")
 CORS(app)
-cache = Cache(app, config={"CACHE_TYPE": "filesystem", "CACHE_DIR": "./"})
+cache = Cache(app, config={"CACHE_TYPE": "filesystem", "CACHE_DIR": "./cache/"})
 
 
 @app.route('/<path:filename>')
@@ -38,24 +40,38 @@ def mirador():
             manifest = request.args.get("manifest", default=None, type=str)
             canvas = request.args.get("canvas", default=None, type=str)
             if manifest:
+                manifest_json = None
+                m = requests.get(manifest)
+                if m.status_code == requests.codes.ok:
+                    manifest_json = m.json()
+                else:
+                    logging.error('The manifest request returned %s', m.status_code)
+                    flask.abort(m.status_code)
                 if not canvas:
-                    m = requests.get(manifest)
-                    if m.status_code == requests.codes.ok:
-                        manifest_json = m.json()
+                    if manifest_json:
                         try:
+                            logging.debug('Using first canvas by default.')
                             canvas = manifest_json["sequences"][0]["canvases"][0]["@id"]
-                            print(canvas)
                         except KeyError:
+                            logging.error('Could not identify first canvas in manifest')
                             flask.abort(500)
                     else:
+                        logging.error('No manifest available.')
                         flask.abort(404)
-            css_uri = url_for('static', filename='mirador/css/mirador-combined.css')
-            min_js_uri = url_for('static', filename='mirador/mirador.min.js')
-            template_result = t.render(manifest_uri=manifest, canvas_uri=canvas, css=css_uri, min=min_js_uri)
+            template_result = t.render(manifest_uri=manifest, canvas_uri=canvas)
             return flask.Response(template_result)
+        else:
+            logging.error('Unsupported HTTP method.')
+            flask.abort(405)
     else:
+        logging.error('Could not find JINJA2 template.')
         flask.abort(500)
 
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        stream=sys.stdout,
+        level=logging.DEBUG,
+        format="%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s",
+    )
     app.run(threaded=True, debug=True, port=5000, host="0.0.0.0")
